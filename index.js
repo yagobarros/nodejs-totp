4c04b41a892b177469f9bdb779aeb19a363a21d3
Yago Barros <yagoregnum@gmail.com>
2019-05-13 17:29:33 -0300

    backend auth init

--- index.js
+++ index.js
@@ -1,24 +1,68 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
+let express = require("express");
+let speakeasy = require("speakeasy")
+let bodyParser = require('body-parser')
+let QRCode = require('qrcode')
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-router.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
 
-router.get('/about', function(req, res) {
-  res.sendFile(path.join(__dirname + '/about.html'));
-});
+let user =  {
+        email: 'email@email.com',
+        password: '12345',
+        secret_key: 'JQ3WGM3LN5XCK42PEFNGYOT2JM',
+        two_factor: true
+    }
 
-router.get('/sitemap', function(req, res) {
-  res.sendFile(path.join(__dirname + '/sitemap.html'));
-});
+let generateKey = (req, res) => {
+    let secret = speakeasy.generateSecret({ length: 16 });
+    QRCode.toDataURL(secret.otpauth_url, (err, data_url)=>{
+        console.log(data_url)
+        res.send("<img src='"+data_url+"'>" + secret.base32);
 
-//add the router
-app.use('/', router);
-app.listen(process.env.port || 3000);
+    }) 
+}
 
-console.log('Running at Port 3000');
+app.get("/", (req, res)=>{ 
+    res.send("")    
+})
+
+app.get("/key", generateKey)
+
+app.get("/login", (req, res) => {
+    res.send("<html><body><form action='/logar' method='post'> <label>email:</label><input type='email' name='email'><label>senha</label><input type='password' name='password'><input type='submit' value='Login'></form></body></html>"
+        
+    )
+} )
+
+app.post('/logar', (req, res)=>{
+    let data = req.body
+    if(data.email == user.email && data.password == user.password) {
+        if(user.two_factor) {
+            res.send("<html><body><form action='/access' method='post'> <label>key:</label><input type='text' name='key'><input type='submit' value='Login'></form></body></html>")
+        }
+        
+        res.send('login succeed')
+    } else { 
+        res.send("falhou")
+    }
+})
+
+
+app.post('/access', (req, res)=>{
+    let data = req.body
+    let verified = speakeasy.totp.verify({secret: user.secret_key, encoding
+    : 'base32', token: data.key})
+    if(verified){
+        res.send('login sucesso')
+    } else {
+        res.send("login failed")
+    }
+})
+
+
+app.listen(3000, ()=>{
+    console.log("running...")
+})
